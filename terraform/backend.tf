a5c97262733cb02e22cd05bf16fc692f2895876f
alissento <knez.norbert@gmail.com>
2025-04-13 15:43:47 +0200

    changed backend to terraform cloud

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,10 @@
-terraform { // Define the Terraform backend (for state management), later we can change it Fontys AWS account
-  backend "s3" {
-    bucket  = "nkterraform"
-    key     = "terraform/terraform.tfstate"
-    region  = "eu-central-1"
-    encrypt = true
+terraform {
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "DC-Fontys-S4"
+
+    workspaces {
+      name = "distributed-computing-fontys-s4"
+    }
   }
 }
