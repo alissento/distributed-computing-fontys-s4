08d8ccf1e41fc98bfe376e6c1b97562f0904520e
alissento <knez.norbert@gmail.com>
2025-04-22 10:51:17 +0200

    s3 fix

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -19,11 +19,11 @@ resource "aws_s3_bucket_versioning" "versioning" {
 resource "aws_s3_object" "ansible_worker_node_script" {
   bucket = aws_s3_bucket.kubernetes_bucket.id
   key    = "ansible/k8_worker.yml"
-  source = "ansible/k8_worker.yml"
+  source = "ansible/workerplaybook.yml"
 }
 
 resource "aws_s3_object" "ansible_master_node_script" {
   bucket = aws_s3_bucket.kubernetes_bucket.id
   key    = "ansible/k8_master.yml"
-  source = "ansible/k8_master.yml"
+  source = "ansible/k8book.yml"
 }
