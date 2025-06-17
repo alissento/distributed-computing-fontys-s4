ce8be749ef1b94f86686da31bcd948632ba66efb
Owen Wal <ownvndwl@gmail.com>
2025-06-17 13:47:49 +0200

    Update handle_get_all_jobs.go

--- Api&Processing/go-api/handle_get_all_jobs.go
+++ Api&Processing/go-api/handle_get_all_jobs.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"strings"
 )
 
 func getAllJobs(w http.ResponseWriter, r *http.Request) {
-	allJobsData, err := ListS3Keys("jobs") //Todo: hardcode bucket name
+	log.Println("This is the jobBucket:", jobBucket)
+	allJobsData, err := ListS3Keys(jobBucket)
 	if err != nil {
 		http.Error(w, "Failed to fetch jobs: "+err.Error(), http.StatusInternalServerError)
 		return
